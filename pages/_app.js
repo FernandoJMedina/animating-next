d3da9ea5e977571d2a90bd8a25bbf59e018cc615
Fernando Medina <f.medina@paisanoscreando.com>
2021-05-06 13:32:58 -0300

    check integrations with next

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import "../styles/globals.css";
+import Scrollbar from "smooth-scrollbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	useEffect(() => {
+		if (typeof window !== "undefined" && typeof document !== "undefined") {
+			Scrollbar.init(document.querySelector("#test"));
+		}
+	}, []);
+	return (
+		<div id="test">
+			<Component {...pageProps} />;
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
